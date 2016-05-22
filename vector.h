fdd1b453c378883be49f47e8013eceaa80a6d1fb
Joran Bigalet <joran.bigalet@gmail.com>
2016-05-22 16:28:11 +0200

    Working vector class

--- vector.h
+++ vector.h
@@ -21,65 +21,31 @@ class Vector {
     inline Vector operator+() const { return *this; }
     inline Vector operator-() const { return Vector(-x, -y, -z); }
 
-    inline Vector operator+(const Vector &v1, const Vector &v2) {
-      return Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
-    }
-
-    inline Vector operator-(const Vector &v1, const Vector &v2) {
-      return Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
-    }
-
-    inline Vector operator*(const float f, const Vector &v) {
-      return Vector(v.x * f, v.y * f, v.z * f);
-    }
-    inline Vector operator*(const Vector &v, const float f) { return f*v; }
-
-    inline Vector operator/(const float f, const Vector &v) {
-      return Vector(v.x / f, v.y / f, v.z / f);
-    }
-
-
-// Special operators
-
-    // dot product
-    inline float operator%(const Vector &v1, const Vector &v2) {
-      return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
-    }
-
-    // cross product
-    inline Vector operator*(const Vector &v1, const Vector &v2) {
-      return Vector(
-         v1.y*v2.z - v1.z*v2.y,
-        -v1.x*v2.z + v1.z*v2.x,
-         v1.x*v2.y - v1.y*v2.x,
-      );
-    }
-
 
 // Assignment operators
 
-    inline Vector operator+=(const Vector &v2) const {
+    inline Vector operator+=(const Vector &v2) {
       x += v2.x;
       y += v2.y;
       z += v2.z;
       return *this;
     }
 
-    inline Vector operator-=(const Vector &v2) const {
+    inline Vector operator-=(const Vector &v2) {
       x -= v2.x;
       y -= v2.y;
       z -= v2.z;
       return *this;
     }
 
-    inline Vector operator*=(const float f) const {
+    inline Vector operator*=(const float f) {
       x *= f;
       y *= f;
       z *= f;
       return *this;
     }
 
-    inline Vector operator/=(const float f) const {
+    inline Vector operator/=(const float f) {
       x /= f;
       y /= f;
       z /= f;
@@ -87,5 +53,44 @@ class Vector {
     }
 
 };
+
+
+// Operators
+
+inline Vector operator+(const Vector &v1, const Vector &v2) {
+  return Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+
+inline Vector operator-(const Vector &v1, const Vector &v2) {
+  return Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
+inline Vector operator*(const float f, const Vector &v) {
+  return Vector(v.x * f, v.y * f, v.z * f);
+}
+inline Vector operator*(const Vector &v, const float f) { return f*v; }
+
+inline Vector operator/(const float f, const Vector &v) {
+  return Vector(v.x / f, v.y / f, v.z / f);
+}
+
+
+// Special operators
+
+// dot product
+inline float operator%(const Vector &v1, const Vector &v2) {
+  return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
+}
+
+// cross product
+inline Vector operator*(const Vector &v1, const Vector &v2) {
+  return Vector(
+      v1.y*v2.z - v1.z*v2.y,
+      -v1.x*v2.z + v1.z*v2.x,
+      v1.x*v2.y - v1.y*v2.x
+  );
+}
+
+
 
 #endif /* DEF_VECTOR */
