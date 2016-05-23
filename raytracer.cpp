0aebe8db5dea08f02efde342d25d385e4b2c060b
Joran Bigalet <joran.bigalet@gmail.com>
2016-05-24 01:56:08 +0200

    monofile speedup

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,6 +1,10 @@
+#include <xmmintrin.h>
+
+
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <cmath>
 
 #include "vector.h"
 #include "camera.h"
@@ -8,12 +12,14 @@
 #include "utils.h"
 
 int main() {
+  _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+
   auto startTime = std::chrono::high_resolution_clock::now();
 
-  /* int width = 1600; */
-  /* int height = 800; */
-  int width = 400;
-  int height = 200;
+  int width = 1600;
+  int height = 800;
+  /* int width = 400; */
+  /* int height = 200; */
 
   std::ofstream image;
   image.open("image.ppm");
@@ -51,57 +57,74 @@ int main() {
   for(int j=height-1 ; j>=0; j--)
     for(int i=0 ; i<width ; i++) {
 
-      // TODO samples
-      int bounce = 100;
+      Vector average_color = VECTOR_ZERO;
 
-      Vector ray_orig = orig;
-      Vector ray_dir = lower_left_corner + hor*((i+RANDOM_FLOAT)/width) + ver*((j+RANDOM_FLOAT)/height) - orig;
-      Vector ray_att = VECTOR_ONE;
+      for(int nsample=0 ; nsample<100 ; nsample++){
+
+        Vector ray_orig = orig;
+        Vector ray_dir = lower_left_corner + hor*((i+RANDOM_FLOAT)/width) + ver*((j+RANDOM_FLOAT)/height) - orig;
+
+        Vector ray_color = VECTOR_ONE;
 
 
+        for(int bounce=0; bounce<100; bounce++){
+          Vector oc = ray_orig - sphere_center;
+          float b = 2.0f * oc%ray_dir;
 
+          bool did_hit = false;
+          float hit_time = 0.0f;
 
-      Vector oc = ray_orig - sphere_center;
-      float a = ray_dir%ray_dir;
-      float b = 2.0f * oc%ray_dir;
-      float c = oc%oc - sphere_radius*sphere_radius;
-      float discriminant = b*b-4.0f*a*c;
+          if( b < 0.0f ){
+            did_hit = true;
 
-      float hit_time = 0.0f;
-      bool did_hit = true;
+            float a = ray_dir%ray_dir;
+            float b2 = b*b;
+            float c = oc%oc - sphere_radius*sphere_radius;
+            float discriminant = b2-4.0f*a*c;
 
-      if(discriminant > 0.0f) {
-        float hit_time = (-b-sqrt(discriminant))/(2.0f*a);
-        /* if(hit_time < t_min || hit_time > t_max){ */
-        if(hit_time < 0.001f) {
-          hit_time = (-b+sqrt(discriminant))/(2.0f*a);
-          /* if(hit_time < t_min || hit_time > t_max) */
-          if(hit_time < 0.001f)
-            did_hit = false;
+            /* -b-sqrt > € */
+            /* -b > sqrt + € */
+            /* b2 > dis + € if b < 0 */
+            /* b2 < dis + € if b > 0 */
+
+            /* -b+sqrt > € */
+            /* sqrt > b + € */
+            /* dis < b + € if b < 0 */
+
+            if(discriminant > 0.0f) {
+              if(b2 > discriminant + 0.001f) {
+                hit_time = (-b-sqrt(discriminant))/(2.0f*a);
+              } else {
+                if(discriminant < b2 + 0.001f) {
+                  hit_time = (-b+sqrt(discriminant))/(2.0f*a);
+                } else {
+                  did_hit = false;
+                }
+              }
+            } else {
+              did_hit = false;
+            }
+          }
+
+          if(did_hit){
+            Vector ray_point = ray_orig + hit_time*ray_dir;  // TODO remove this
+            ray_orig = ray_point;
+            ray_dir = (ray_point - sphere_center) / sphere_radius + random_point_in_sphere();
+
+            ray_color = ray_color ^ sphere_att;;
+
+          } else {
+            float t = 0.5f*(ray_dir.unit().y + 1.0f);
+            ray_color = ray_color^((1.0-t)*VECTOR_ONE + t*Vector(0.5f, 0.7f, 1.0f));
+            break;
+          }
         }
-      } else {
-        did_hit = false;
+
+        average_color += ray_color;
       }
 
-      Vector color;
-
-      if(did_hit){
-        /* ray_point = ray_orig + tmp*ray_dir; */
-        /* return new HitRecord( */
-        /*     tmp, */
-        /*     ray_point, */
-        /*     (ray_point - sphere_center) / sphere_radius */
-        /*     ); */
-
-        color = VECTOR_ONE;
-
-      } else {
-        float t = 0.5f*(ray_dir.unit().y + 1.0f);
-        color = (1.0-t)*VECTOR_ONE + t*Vector(0.5f, 0.7f, 1.0f);
-      }
-
-
-      int *rgb = color.toRGB(2);
+      average_color = average_color / 100.0f;
+      int *rgb = average_color.toRGB(2);
       image << rgb[0] << " " << rgb[1] << " " << rgb[2] << "\n";
     }
 
